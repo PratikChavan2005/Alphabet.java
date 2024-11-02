efa42c2bfd7215e204f06179bf318a4e5b269eeb
PratikChavan2005 <pratikchavan963744@gmail.com>
2024-11-02 13:46:12 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,24 @@
+class Alphabet
+{
+   
+    public static void main(String[] args)
+   
+   {
+	char i;
+        System.out.println("Lowercase:");
+        for(i='a';i<='z';i++)
+	{
+           System.out.println(i+ " ");
+        }
+             
+          System.out.println();
+        
+           System.out.println("Uppercase");
+           for(i='A';i<='Z';i++){
+             System.out.println(i+"");
+      }         
+
+   }
+
+}
+
